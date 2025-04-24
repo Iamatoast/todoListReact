bee030aa3a5ee85d034cac4e3f164fff4ab8b803
Iamatoast <neuyv@protonmail.com>
2025-04-24 09:01:31 -0300

    elementControl fixed

--- src/elementControl.jsx
+++ src/elementControl.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './ElementControl.css'
 
-export default function ElementControl({task,setTask}) {
+export default function ElementControl({task, setTask}) {
+  const ref = useRef(null);
+  function insert() {
+    let currentTask = ref.current.value;
+    setTask([...task, currentTask]);
+    console.log(task);
+    ref.value = "";
+  }
 
   return (
     <section id="newElement">
-      <div className="input-group mb-3">
-        <button type="button" className="btn btn-secondary btn-sm" onClick="">New</button>
-        <input type="text" id="text" className="form-control" placeholder="Nueva tarea" aria-label="Username" aria-describedby="basic-addon1"></input>
-      </div>
+        <div className="input-group mb-3">
+          <button type="button" className="btn btn-secondary btn-sm" onClick={insert}>New</button>
+          <input type="text" ref={ref} id="text" className="form-control" placeholder="Nueva tarea" aria-label="Username" aria-describedby="basic-addon1"></input>
+        </div>
     </section>
   )
 }
